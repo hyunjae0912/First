983e552f7c24d6f8fdec2cd4abc3e640f243a52c
hyun jae <hyunjae00912@naver.com>
2025-02-13 10:38:24 +0900

    새로운 프로젝트 생성

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,18 @@
+package hello;
+
+/*
+ * 작성자 : 유현재
+ * 작성일 : 2/12
+ */
+public class HelloJava {
+	
+	// 메인함수: 프로그램의 시작점
+	public static void main(String[] args) {
+		
+		// 문장을 출력하는 코드
+		// 문장을 완성할 때는 무조건 꼭 ;(세미콜론)으로 마무리 하기
+		System.out.println("Hello, World!!");
+		
+	}
+
+}
