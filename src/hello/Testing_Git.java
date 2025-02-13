794f95abff0da75537ce1909a9096f3c525cb8f7
hyun jae <hyunjae00912@naver.com>
2025-02-13 11:16:18 +0900

    깃허브 테스트중

---
+++ src/hello/Testing_Git.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Testing_Git {
+
+	public static void main(String[] args) {
+		
+		//언녕 내 새상
+		for(int i = 0; i < 10; i ++) {
+			System.out.println("Hello, My world!!");
+		}
+	}
+
+}
